b9df59ad7287c65e41853685c3cf7a127213327b
CharlieRoot <wieger.bontekoe@gmail.com>
2020-09-12 08:13:48 +0200

    Signed-off-by: CharlieRoot <wieger.bontekoe@gmail.com>

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Profile
+from .models import ResourceIPv4
+from .models import ResourceIPv6, ResourceASN, Peers
 
 admin.site.register(Profile)
+admin.site.register(ResourceIPv4)
+admin.site.register(ResourceIPv6)
+admin.site.register(ResourceASN)
+admin.site.register(Peers)
+
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -32,4 +32,50 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
+
+
+
+class ResourceIPv4(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource_prefix = models.CharField(max_length=500, blank=True)
+    resource_country = models.CharField(max_length=50,choices=COUNTRY_CHOICES,null=True)
+    resource_netname = models.CharField(max_length=500, blank=True)
+    resource_description = models.CharField(max_length=500, blank=True)
+    resource_organisation = models.CharField(max_length=500, blank=True)
+    resource_admin_c = models.CharField(max_length=500, blank=True)
+    resource_tech_c = models.CharField(max_length=500, blank=True)
+    resource_mnt_lower = models.CharField(max_length=500, blank=True)
+    resource_mnt_routes = models.CharField(max_length=500, blank=True)
+    resource_mnt_domains = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ResourceIPv6(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource_prefix = models.CharField(max_length=500, blank=True)
+    resource_country = models.CharField(max_length=50,choices=COUNTRY_CHOICES,null=True)
+    resource_netname = models.CharField(max_length=500, blank=True)
+    resource_description = models.CharField(max_length=500, blank=True)
+    resource_organisation = models.CharField(max_length=500, blank=True)
+    resource_admin_c = models.CharField(max_length=500, blank=True)
+    resource_tech_c = models.CharField(max_length=500, blank=True)
+    resource_mnt_lower = models.CharField(max_length=500, blank=True)
+    resource_mnt_routes = models.CharField(max_length=500, blank=True)
+    resource_mnt_domains = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ResourceASN(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource_name = models.CharField(max_length=500, blank=True)
+    resource_country = models.CharField(max_length=50,choices=COUNTRY_CHOICES,null=True)
+    resource_organisation = models.CharField(max_length=500, blank=True)
+    resource_website = models.CharField(max_length=500, blank=True)
+
+class Peers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    peer_asn = models.ForeignKey(ResourceASN, on_delete=models.CASCADE)
+    remote_asn = models.CharField(max_length=500, blank=True)
+    peer_email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
